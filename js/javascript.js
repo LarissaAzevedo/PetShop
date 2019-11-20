02ad6d6f5cbb65fb70c5b18bc10f118b09916591
Larissa Aleixos <Larissa@MacBook-Pro-de-Alessandro.local>
2019-11-20 21:05:07 -0200

    Junção das partes

--- js/javascript.js
+++ js/javascript.js
@@ -8,10 +8,25 @@ cCao = document.getElementById("cao")
 cCard = document.getElementById("card")
 
 // chamada do texto - página de informações
-function abreCard(event){
+function abreCard(event) {
     cCard.classList.add('texto_info_abre')
     cCard.classList.remove('texto_info')
-    cCard.classList.add('animated','bounceInDown')
+    cCard.classList.add('animated', 'bounceInDown')
     event.preventDefault()
 
+}
+
+
+// PAG ADOÇÃO
+
+txtImg = document.getElementById("txt1")
+txtImg = document.getElementById("txt2")
+txtImg = document.getElementById("txt3")
+
+function hover() {
+    txtImg.classList.remove('text_img1')
+}
+
+function removeHover() {
+    txtImg.classList.add('text_img1')
 }
